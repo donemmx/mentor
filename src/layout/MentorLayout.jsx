9ad6baa25d100454d87320c6fca571b730b2cda5
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-09-22 10:00:28 +0100

    fix

--- src/layout/MentorLayout.jsx
+++ src/layout/MentorLayout.jsx
@@ -1,10 +1,12 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { authState } from "../atom/authAtom";
+import { workspaceStore } from "../atom/workspaceAtom";
 
 export default function MentorLayout() {
   let auth = useRecoilValue(authState);
-  
+  const navigate = useNavigate();
+  const workspaceData = useRecoilValue(workspaceStore)
   return (
     <div className="">
     {auth && auth?.role === 'mentor' ? (
@@ -13,8 +15,8 @@ export default function MentorLayout() {
       </div>
     ) : (
       <>
-        <Outlet />
-      </>
+          {navigate(`/mentor-signin/${workspaceData?.workspace}`)}
+        </>
     )}
   </div>
   );
