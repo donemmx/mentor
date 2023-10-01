36e1930322a8f07a5f9fdfb2827ff1c2c3b36181
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-10-02 00:38:47 +0100

    fix

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { authState } from "../atom/authAtom";
+
 export default function DefaultLayout() {
   let auth = useRecoilValue(authState);
 
