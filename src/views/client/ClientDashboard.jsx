34f08d1721cd6aa9b48f5c59b065e766a0a02fee
donemmx <emmanuelidus@gmail.com>
2023-10-21 06:15:02 +0100

    fix

--- src/views/client/ClientDashboard.jsx
+++ src/views/client/ClientDashboard.jsx
@@ -20,22 +20,14 @@ export default function ClientDashboard() {
       console.log(res);
     }).catch((err)=> console.log(err))
   }, [])
-  // console.log(auth, 'User\n\n')
-  // console.log(auth.username, 'User id\n\n')
-  // console.log(workspace.id, 'workspace\n\n')
-  // console.log(workspace, 'workspace\n\n')
+ 
   return (
     <div>
       <ClientSidebar />
       <div className="w-[90%] mx-auto flex gap-10">
         <div className=" mt-5 space-y-7 border broder-gray-100 p-6 w-fit rounded">
           <div className="">Recent Notifications</div>
-          {/* <Notification />
-          <Notification />
-          <Notification />
-          <Notification />
-          <Notification />
-          <Notification /> */}
+        
         </div>
         <div className="mt-5 p-6">
           <div className="mb-5">Recently Registered</div>
