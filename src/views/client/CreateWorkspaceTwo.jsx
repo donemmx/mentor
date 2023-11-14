8840ec8dfb1605c28c42817c7075c50260fcce12
donemmx <emmanuelidus@gmail.com>
2023-11-14 16:27:20 +0100

    fix

--- src/views/client/CreateWorkspaceTwo.jsx
+++ src/views/client/CreateWorkspaceTwo.jsx
@@ -55,7 +55,6 @@ export default function CreateWorkspaceTwo() {
       _provinceId: reg.user.province,
       _postalcode: reg.user.postalcode,
       _action: "createWithPayment",
-      _actionType_input: true,
       _url: `${window.location.origin}/payments/${reg.workspace.id}`,
     };
 
